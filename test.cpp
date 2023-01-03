cf9fe2440753e6db2e4bf905951ff12023d1dce2
IhorTiupov <igor.tyupov@gmail.com>
2023-01-03 18:11:09 +0200

    Add TestMoveTank(Left, Right, Up, down)

--- test.cpp
+++ test.cpp
@@ -63,33 +63,6 @@ protected:
 
 };
 
-TEST_F(TankTest, TestTankPosition)
-{
-		for (size_t i = height / 2; i < height / 2 + heightDimensionsTank; i++)
-		{
-			for (size_t j = width / 2; j < width / 2 + widthDimensionsTank; j++)
-			{
-				EXPECT_EQ('X', gameField.getField()[i][j]);
-			}
-		}
-		EXPECT_EQ('H', gameField.getField()[heightDimensionsTank - 2 + height / 2][widthDimensionsTank + width / 2]);
-}
-
-
-TEST_F(TankTest, TestMoveTankLeft)
-{
-	tank.move(MoveType::Left);
-	tank.draw(gameField.getField());
-	for (size_t i = height / 2; i < height / 2 + heightDimensionsTank; i++)
-	{
-		for (size_t j = width / 2-1; j < width / 2 + widthDimensionsTank-1; j++)
-		{
-			EXPECT_EQ('X', gameField.getField()[i][j]);
-		}
-	}
-	//EXPECT_EQ('H', gameField.getField()[heightDimensionsTank - 2 + height / 2 +1][widthDimensionsTank + width / 2 -1 -1]);
-}
-
 class ThrowTest : public ::testing::Test
 {
 public:
@@ -107,7 +80,6 @@ public:
 	}
 
 };
-
 
 TEST_F(ThrowTest, TestThrowRuntimeExeption)
 {
@@ -145,7 +117,6 @@ TEST_F(FieldTest, TestFullingField)
 		}
 	}
 }
-
 TEST_F(WallTest, TestLeftWall)
 {
 	for (std::vector<char>& i : gameField.getField())
@@ -172,5 +143,66 @@ TEST_F(WallTest, TestDownWall)
 	for (char i : gameField.getField()[height-1])
 	{
 		EXPECT_EQ('*', i);
+	}
+}
+TEST_F(TankTest, TestTankPosition)
+{
+	for (size_t i = height / 2; i < height / 2 + heightDimensionsTank; i++)
+	{
+		for (size_t j = width / 2; j < width / 2 + widthDimensionsTank; j++)
+		{
+			EXPECT_EQ('X', gameField.getField()[i][j]);
+		}
+	}
+	EXPECT_EQ('H', gameField.getField()[heightDimensionsTank - 2 + height / 2][widthDimensionsTank + width / 2]);
+}
+TEST_F(TankTest, TestTankMoveRight)
+{
+	tank.move(MoveType::Right);
+	tank.draw(gameField.getField());
+	for (size_t i = height / 2; i < height / 2 + heightDimensionsTank; i++)
+	{
+		for (size_t j = width / 2 + 1; j < width / 2 + widthDimensionsTank + 1; j++)
+		{
+			EXPECT_EQ('X', gameField.getField()[i][j]);
+		}
+	}
+}
+TEST_F(TankTest, TestTankMoveLeft)
+{
+	tank.move(MoveType::Left);
+	tank.draw(gameField.getField());
+	for (size_t i = height / 2; i < height / 2 + heightDimensionsTank; i++)
+	{
+		for (size_t j = width / 2 - 1; j < width / 2 + widthDimensionsTank - 1; j++)
+		{
+			EXPECT_EQ('X', gameField.getField()[i][j]);
+		}
+	}
+
+	//EXPECT_EQ('H', gameField.getField()[heightDimensionsTank - 2 + height / 2 + 1][widthDimensionsTank + width / 2 -1 -1]);
+}
+TEST_F(TankTest, TestTankMoveUp)
+{
+	tank.move(MoveType::Up);
+	tank.draw(gameField.getField());
+	for (size_t i = height / 2 - 1; i < height / 2 + heightDimensionsTank - 1; i++)
+	{
+		for (size_t j = width / 2; j < width / 2 + widthDimensionsTank; j++)
+		{
+			EXPECT_EQ('X', gameField.getField()[i][j]);
+		}
+	}
+}
+TEST_F(TankTest, TestTankMoveDown)
+{
+	tank.move(MoveType::Down);
+	tank.draw(gameField.getField());
+	for (size_t i = height / 2 + 1; i < height / 2 + heightDimensionsTank + 1; i++)
+	{
+		for (size_t j = width / 2; j < width / 2 + widthDimensionsTank; j++)
+		{
+			EXPECT_EQ('X', gameField.getField()[i][j]);
+		}
 	}
 }
